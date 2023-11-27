4de302640199cd69152a07eba410171174ddac35
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-27 14:35:03 +0100

    Add project files.

---
+++ Ch09Ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ch09Ex01
+{
+    internal class Program
+    {
+        public abstract class MyBase {}
+        internal class MyClass : MyBase {}
+        public interface IMyBaseInterface {}
+        internal interface IMyBaseInterface2 {}
+        internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2 {}
+        internal sealed class MyComplexClass : MyClass, IMyInterface {}
+        static void Main(string[] args)
+        {
+            MyComplexClass myObj = new MyComplexClass();
+            Console.WriteLine(myObj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
